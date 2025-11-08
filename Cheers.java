7a051b510198e3c653de55b13f98e88a106c38d1
shay1509 <shay.amir01@post.runi.ac.il>
2025-11-08 22:58:10 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,28 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0];
+            int num = Integer.parseInt(args[1]);
+            String condition = "AEFHILMNORSX";
+            int i = 0;
+            int j = 0;
+        
+            word = word.toUpperCase();
+            while(i < word.length()){
+                char letter = word.charAt(i);
+                if(condition.indexOf(letter) != -1){
+                        System.out.print("Give me an ");
+                }
+                else{
+                        System.out.print("Give me a  ");
+                }
+                System.out.println(letter + ": " + letter + "!");
+                i++;
+            }
+
+            while(j < num){
+                System.out.println(word + "!!!");
+                j++;
+            }
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,44 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int N = Integer.parseInt(args[0]);
+		String mode = args[1];
+		int seed = 1;
+		int num = seed;
+		int i = 0;
+		int count = 1;
+
+		while(seed <= N){
+			if(mode.equals("v")){
+				System.out.print(seed + " ");
+			}
+			while(i != 1){
+				if(num % 2 == 0){
+					num = num / 2;
+				}
+				else {
+					num = num * 3 + 1;
+				}
+				if(mode.equals("v")){
+					System.out.print(num + " ");
+				}
+				i = num;
+				count++;
+			}
+			i = 0;
+			seed++;
+			num = seed;
+			if(mode.equals("v")){
+				System.out.println("(" + count + ")");
+			}
+			count = 1;
+			}
+
+		if(mode.equals("v")){
+				System.out.println();
+		}
+		
+		System.out.println("Every one of the first " + N 
+							+ " hailstone sequences reached 1.");
 	}
 }
